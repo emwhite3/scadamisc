b86ac46016e6512e9f75e18dce5ed6921a82573e
emwhite3 <emwhitee3@miners.utep.edu>
2020-01-22 09:57:38 -0700

    First version of PI TCP server

--- PLC_Host.py
+++ PLC_Host.py
@@ -1,26 +1,46 @@
 import socket
+from socket import gethostbyname
 import sys
+import os
 
+def get_val(sens):
+	return str(50)
+
+
+
+# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ("169.254.165.142", 5000)
+# Bind the socket to the port
+hostName = gethostbyname('0.0.0.0')
+port = 5004
+server_address = (hostName, port)
+print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
+# Listen for incoming connections
 sock.listen(1)
 
 while True:
-    print("waiting for connection")
+    # Wait for a connection
+    print ('waiting for a connection')
     connection, client_address = sock.accept()
+	try:
+		print ('connection from %s', client_address)
+		break
 
+
+while True:
     try:
-        print("connection from %s", client_address)
-        
-        while True:
-            data = connection.recv(1024)
-            print("recieved %s", data)
-            if data:
-                connection.sendall(data)
-            else:
-                break
+        sens = connection.recv(1024)
+		print(sens)
+        print('Getting value for %s' % sens)
+		connection.sendall(get_val(sens))
+	
+	except KeyboardInterrupt:
+		connection.close()
+		os._exit(1)  
+		    
     finally:
+        # Clean up the connection
         connection.close()
